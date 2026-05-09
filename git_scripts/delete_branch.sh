09ca827888dee2aa684b33ddd25d11585a0ed424
Dana Wilson <drw5767@psu.edu>
2026-05-09 06:04:22 -0400

    Added script to remove local branches

---
+++ git_scripts/delete_branch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+branch="$1"
+
+if [ -z "$branch" ]; then
+  echo "Usage: ./delete_branch.sh <branch>"
+fi
+current_dir=$(pwd)
+
+# update remote-tracking branches.
+git fetch --prune
+
+# Check if remote branch exists
+if git ls-remote --heads origin "$branch" >/dev/null 2>&1; then
+  echo "Remote branch '${branch}' still exists. Not deleting."
+else
+  echo "Remote branch '${branch}' does NOT exist. Deleting local branch..."
+  git branch -d "$branch" 2>/dev/null || git branch -D "$branch"
+fi
+
